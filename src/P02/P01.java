61508c56aed8da008041d4e3018ce34b2ec938b2
furkanbhc23 <furkanbhc23@gmail.com>
2024-11-17 16:17:54 +0200

    second commit

--- src/P02/P01.java
+++ src/P02/P01.java
@@ -1,9 +1,20 @@
 package P02;
 
+import java.util.Scanner;
+
 public class P01 {
     public static void main(String[] args) {
         int a =128;
         int b =17;
         System.out.println("c=" + (a+b));
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lutfen kullanıcı bilgilerinizi giriniz.");
+        int sayı=scanner.nextInt();
+
+        if (sayı%2==0) {
+            System.out.println("Girdiginiz sayı cift sayıdır.");
+        } else
+            System.out.println("Girdiginiz sayı tektir.");
     }
 }
